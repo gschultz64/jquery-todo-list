09fa483912b047bc53a06c90bad3239051e55e8e
Genevieve <genschultz@gmail.com>
2018-05-29 23:56:34 -0700

    updated files

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
 function addToList(e) {
   e.preventDefault();
   var formData = $('form').serializeArray();
-  $('.to-do').append("<li>" + formData[0].value + "</li><li>" + formData[1].value + "</li><li>" + formData[2].value + "</li>");
+  $('.to-do').append("<li><input type='checkbox'/>" + formData[0].value + "</li><li><input type='checkbox'/>" + formData[1].value + "</li><li><input type='checkbox'/>" + formData[2].value + "</li>");
   
   $('input')[0].value = '';
   $('input')[1].value = '';
